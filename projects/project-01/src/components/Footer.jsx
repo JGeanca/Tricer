df22e78817a586b94638dbcd6bac3d7c65f10b82
JGeanca <geancarlorivera831@gmail.com>
2024-09-19 10:59:37 -0600

    Adding social media Icons

--- projects/project-01/src/components/Footer.jsx
+++ projects/project-01/src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import { InstagramIcon, FacebookIcon, XIcon, TiktokIcon, PinterestIcon } from '../assets/Icons'
+
 export function Footer() {
   return (
     <footer>
       <hr />
+      <div className='icons'>
+        <PinterestIcon />
+        <InstagramIcon />
+        <FacebookIcon />
+        <TiktokIcon />
+      </div>
+
       <p>Footer miedo &copy; 2024 Tricer</p>
     </footer>
   )
