81739c15f0c4c759589f9d31a01651533cf9e6d6
sebastianrodrigueztencio <sebastian.rodrigueztencio@ucr.ac.cr>
2024-09-20 21:20:55 -0600

    Header added without dropdown menus.

--- projects/project-01/src/components/Header.jsx
+++ projects/project-01/src/components/Header.jsx
@@ -1,14 +1,31 @@
 import { Link } from 'react-router-dom'
-import { CartIcon } from '../assets/Icons'
+import { CartIcon, ShopTitle, ProfileIcon } from '../assets/Icons'
+import '../css/header.css'
 
 export function Header() {
   return (
-    <header>
-      <p>This is the Header!</p>
-      <CartIcon />
-      <nav>
-        <Link to="/">HomePage</Link>
-        <Link to="/men">MenPage</Link>
+    <header className="header-container">
+      <nav className="header-nav">
+        <div className="header-left">
+          <Link to="/women" className="header-link">Women</Link>
+          <Link to="/men" className="header-link">Men</Link>
+        </div>
+        <div className="header-center">
+          <Link to="/" className="header-logo">
+            <ShopTitle />
+          </Link>
+        </div>
+        <div className="header-right">
+          <div className="header-search">
+            <input type="search" placeholder="Search" />
+          </div>
+          <Link to="/cart" className="header-cart">
+            <CartIcon />
+          </Link>
+          <Link to="/profile" className="header-profile">
+            <ProfileIcon />
+          </Link>
+        </div>
       </nav>
     </header>
   )
