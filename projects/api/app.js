bf1f85387efd055bfcd52df2d60b0a7151c9915c
JGeanca <geancarlorivera831@gmail.com>
2024-10-18 10:08:40 -0600

    Morgan logger added

--- projects/api/app.js
+++ projects/api/app.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express'
+import logger from 'morgan'
 import cors from 'cors'
 import { productsRouter } from './routes/products.js'
 
 
 const app = express()
+app.use(logger('dev'))
 app.use(cors())
 app.use(json())
 app.disable('x-powered-by')
