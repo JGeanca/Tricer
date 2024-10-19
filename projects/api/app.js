d92608e859f08506d84679a7da011e808bb1a1e9
JGeanca <geancarlorivera831@gmail.com>
2024-10-19 15:09:46 -0600

    Dependencies inyection pattern added to products resource

--- projects/api/app.js
+++ projects/api/app.js
@@ -1,30 +1,29 @@
 import express, { json } from 'express'
 import logger from 'morgan'
 import cors from 'cors'
-import { productsRouter } from './routes/products.js'
+import { createProductsRouter } from './routes/products.js'
 import { usersRouter } from './routes/users.js'
 import { mainRouter } from './routes/main.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 
 import dotenv from 'dotenv'
 
-dotenv.config()
+export const createApp = ({ productModel }) => {
+  dotenv.config()
+  const app = express()
+  app.use(logger('dev'))
+  app.use(cors())
+  app.use(json())
+  app.use(errorHandler)
+  app.disable('x-powered-by')
 
-const app = express()
-app.use(logger('dev'))
-app.use(cors())
-app.use(json())
-app.use(errorHandler)
-app.disable('x-powered-by')
+  app.use('/', mainRouter)
+  app.use('/products', createProductsRouter({ productModel }))
+  app.use('/users', usersRouter)
 
-app.use('/', mainRouter)
-app.use('/products', productsRouter)
-app.use('/users', usersRouter)
+  const PORT = process.env.PORT ?? 3000
 
-
-
-const PORT = process.env.PORT ?? 3000
-
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`)
+  })
+}
--- projects/api/controllers/products.js
+++ projects/api/controllers/products.js
@@ -1,20 +1,22 @@
-import { ProductModel } from '../models/local-file-system/products.js'
-
 export class ProductController {
+  constructor({ productModel }) {
+    this.productModel = productModel
+  }
+
   //* We assume that the logic if the request returns a empty array is handled in
   //* the frontend
   //* But we can handle 404 errors if the product is not found
   // TODO: Make the model return the error but the result too for more robustness
 
-  static async getAll(req, res) {
+  getAll = async (req, res) => {
     const { gender, type, new: isNew } = req.query
-    const products = await ProductModel.getAll({ gender, type, isNew })
+    const products = await this.productModel.getAll({ gender, type, isNew })
     res.json(products)
   }
 
-  static async getById(req, res) {
+  getById = async (req, res) => {
     const { id } = req.params
-    const product = await ProductModel.getById({ id })
+    const product = await this.productModel.getById({ id })
     return res.json(product)
   }
 }
