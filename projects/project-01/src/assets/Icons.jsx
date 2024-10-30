d34e775ffbed578ec856534a35906842755cb377
sebastianrodrigueztencio <sebastian.rodrigueztencio@ucr.ac.cr>
2024-10-30 12:31:38 -0600

    Improvement header icons and layout.

--- projects/project-01/src/assets/Icons.jsx
+++ projects/project-01/src/assets/Icons.jsx
@@ -4,7 +4,7 @@
 
 import shopSvg from '../assets/icons/shop.svg'
 import profileSvg from '../assets/icons/profile.svg'
-import cartSvg from '../assets/icons/cart.svg'
+import cartSvg from '../assets/icons/basket.svg'
 
 export function ShopTitle() {
   return (
@@ -23,8 +23,8 @@ export function ProfileIcon() {
     <img
       src={profileSvg}
       alt="Profile Icon"
-      width="50"
-      height="50"
+      width="45"
+      height="45"
       className="size-6"
     />
   )
@@ -35,8 +35,8 @@ export function CartIcon() {
     <img
       src={cartSvg}
       alt="Cart Icon"
-      width="32"
-      height="31"
+      width="30"
+      height="30"
       className="size-6"
     />
   )
@@ -52,11 +52,11 @@ export function InstagramIcon({ width = 48, height = 48 }) {
   )
 }
 export function GoogleIcon({ width = 48, height = 48 }) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24"><g fill="none"><g clipPath="url(#akarIconsGoogleContainedFill0)">
-        <path fill="currentColor" fillRule="evenodd" d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0m.14 19.018c-3.868 0-7-3.14-7-7.018s3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585q.134.704.135 1.474c0 4.01-2.677 6.86-6.72 6.86" clipRule="evenodd"/></g><defs><clipPath id="akarIconsGoogleContainedFill0">
-        <path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></g></svg>
-    )
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24"><g fill="none"><g clipPath="url(#akarIconsGoogleContainedFill0)">
+      <path fill="currentColor" fillRule="evenodd" d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0m.14 19.018c-3.868 0-7-3.14-7-7.018s3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585q.134.704.135 1.474c0 4.01-2.677 6.86-6.72 6.86" clipRule="evenodd"/></g><defs><clipPath id="akarIconsGoogleContainedFill0">
+      <path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></g></svg>
+  )
 }
 export function ChevronLeft() {
   return (
