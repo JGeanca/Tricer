78318f4a60f858fa578d6cad24c11f85b4f03cb4
sebastianrodrigueztencio <sebastian.rodrigueztencio@ucr.ac.cr>
2024-10-30 16:38:14 -0600

    Added checkout form and styles for checkout page.

--- projects/project-01/src/pages/CheckoutPage.jsx
+++ projects/project-01/src/pages/CheckoutPage.jsx
@@ -1,17 +1,152 @@
 import '../css/checkout.css'
 import useCartStore from '../stores/cartStore'
+import { useEffect, useState } from 'react'
 
 export default function CheckoutPage() {
   const { items, getCartTotal, getCartItemsCount } = useCartStore()
-  
   const totalItems = getCartItemsCount()
   const totalPrice = getCartTotal()
+  
+  // Hook para manejar la validación del formulario
+  useEffect(() => {
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  }, [])
+
+  const [expiryDate, setExpiryDate] = useState('')
+
+  const handleExpiryChange = (e) => {
+    const input = e.target.value.replace(/[^0-9]/g, '') // Eliminar caracteres no numéricos
+    let formattedValue = ''
+
+    if (input.length > 2) {
+      formattedValue = `${input.slice(0, 2)} / ${input.slice(2, 6)}`
+    } else if (input.length > 0) {
+      formattedValue = input
+    }
+
+    setExpiryDate(formattedValue)
+  }
 
   return (
     <div className="checkout-container">
-      <div className="checkout-form">
-        <h2>Checkout Form</h2>
-      </div>
+      <form className="checkout-form needs-validation" noValidate>
+        <div className="checkout-section">
+          <h3>Contact</h3>
+          <div className="form-floating mb-3">
+            <input type="email" className="form-control" id="floatingEmail" placeholder="Email" name="email" required />
+            <label htmlFor="floatingEmail">Email</label>
+            <div className="invalid-feedback"> Please provide a valid email. </div>
+          </div>
+
+          <div className="checkout-section">
+            <h3>Delivery</h3>
+            
+            <div className="form-floating mb-3">
+              <select className="form-select" name="pais" required>
+                <option value="CR">Costa Rica</option>
+              </select>
+              <label htmlFor="pais">Country</label>
+              <div className="invalid-feedback"> Please select your country.</div>
+            </div>
+
+            <div className="form-row">
+              <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingNombre" placeholder="Nombre" name="nombre" required />
+                <label htmlFor="floatingNombre">First Name</label>
+                <div className="invalid-feedback"> Please provide your first name. </div>
+              </div>
+              <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingApellido" placeholder="Apellido" name="apellido" required />
+                <label htmlFor="floatingApellido">Last Name</label>
+                <div className="invalid-feedback"> Please provide your last name. </div>
+              </div>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" id="floatingInputDireccion" placeholder="Dirección" name="direccion" required />
+              <label htmlFor="floatingInputDireccion">Address</label>
+              <div className="invalid-feedback">Please provide a valid address.</div>
+            </div>
+
+            <div className="form-row">
+              <div className="form-floating mb-3">
+                <select className="form-select" name="provincia" required>
+                  <option value="">Select a province</option>
+                  <option value="CR-A">Alajuela</option>
+                  <option value="CR-C">Cartago</option>
+                  <option value="CR-G">Guanacaste</option>
+                  <option value="CR-H">Heredia</option>
+                  <option value="CR-L">Limón</option>
+                  <option value="CR-P">Puntarenas</option>
+                  <option value="CR-S">San José</option>
+                </select>
+                <label htmlFor="provincia">Province</label>
+                <div className="invalid-feedback"> Please select a province.</div>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingInputCiudad" placeholder="Ciudad" name="ciudad" required />
+                <label htmlFor="floatingInputCiudad">City</label>
+                <div className="invalid-feedback"> Please provide a valid city. </div>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input type="number" className="form-control" id="floatingInputCodigoPostal" placeholder="Código Postal" name="codigoPostal" style={{ appearance: 'none', MozAppearance: 'textfield' }} />
+                <label htmlFor="floatingInputCodigoPostal">Postal Code</label>
+                <div className="invalid-feedback"> Please provide a valid postal code. </div>
+              </div>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="tel" className="form-control" id="floatingCelular" placeholder="Cell Phone" name="celular" required />
+              <label htmlFor="floatingCelular">Phone</label>
+              <div className="invalid-feedback">Please provide a valid phone number.</div>
+            </div>
+          </div>
+
+          <div className="checkout-section payment-section">
+            <h3>Payment Method</h3>
+
+            <div className="form-floating mb-3">
+              <input type="text" id="card-number" className="form-control" placeholder="Card Number" required />
+              <label htmlFor="card-number">Card Number</label>
+            </div>
+
+            <div className="form-row">
+              <div className="form-floating mb-3">
+                <input type="text" id="expiry-date"  className="form-control"  placeholder="MM / YYYY"  value={expiryDate} onChange={handleExpiryChange} pattern="^(0[1-9]|1[0-2]) / [0-9]{4}$" required />
+                <label htmlFor="expiry-date">Expiration date (MM / YYYY)</label>
+                <div className="invalid-feedback">Please provide a valid expiration date (MM / YYYY).</div>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input type="tel"  id="cvv"  className="form-control"  placeholder="CVV"  pattern="^\d{3,4}$" required/>
+                <label htmlFor="cvv">CVV</label>
+                <div className="invalid-feedback">Please provide a valid CVV (3 or 4 digits).</div>
+              </div>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="text" id="card-name" className="form-control" placeholder="Cardholder Name" required />
+              <label htmlFor="card-name">Cardholder Name</label>
+            </div>
+        
+          </div>
+
+          <button type="submit" className="checkout-button">Pay now</button>
+        </div>
+      </form>
+
       <div className="checkout-summary">
         <h2>Order Summary</h2>
         {items.length > 0 ? (
