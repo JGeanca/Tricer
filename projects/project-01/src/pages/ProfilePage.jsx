b5b5f512106f4f5291a482f312889c8537f171aa
JulioAleRodri <julioalejandro03@gmail.com>
2024-11-17 22:37:25 -0600

    Changing the profile page to also have the purchase history

--- projects/project-01/src/pages/ProfilePage.jsx
+++ projects/project-01/src/pages/ProfilePage.jsx
@@ -18,27 +18,51 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className="profile-container">
-      <div className="profile-card">
-        <div className="profile-avatar-container">
-          <img
-            src={user.avatarUrl || profileIcon}
-            alt="User Avatar"
-            className="profile-avatar"
-          />
-        </div>
-        <h1 className="profile-username">
-          Welcome, {user.username}!
-        </h1>
-        <div className="logout-button-container">
-          <button
-            onClick={handleLogout}
-            className="logout-button"
-          >
-            Logout
-          </button>
+    <>
+      <div className="profile-container">
+        <div className="profile-card">
+          <div className="profile-avatar-container">
+            <img
+              src={user.avatarUrl || profileIcon}
+              alt="User Avatar"
+              className="profile-avatar"
+            />
+          </div>
+          <div className="user-data-container">
+            <div className="profile-username">
+              {user.username}
+            </div>
+            <h2 className="profile-email">
+              {user.username}
+            </h2>
+          </div>
+          {/*<div className="extra-container"></div>*/}
+          <div className="profile-buttons-containers">
+            <div className="group-button-container">
+              <button
+                onClick={handleLogout}
+                className="profile-checkout-button"
+              >
+                Checkout
+              </button>
+              <button
+                onClick={handleLogout}
+                className="user-logout-button"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="purchase-history-section">
+        <div className="purchase-history-title">
+          Purchase history
+        </div>
+        <div className="purchases-section">
+          
+        </div>
+      </div>
+    </>
   )
 }
