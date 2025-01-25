c971dcdbb078fba941e01f025ebcde52ec168a26
JGeanca <geancarlorivera831@gmail.com>
2025-01-25 15:38:12 -0600

    Prepare Vercel 5.0 deployment

--- projects/api/index.js
+++ projects/api/index.js
@@ -1,4 +1,4 @@
-import { createApp, startApp } from './app.js'
+// import { createApp, startApp } from './app.js'
 import { pool } from './config/database.js'
 import { ProductModel } from './models/database/products.js'
 import { UserModel } from './models/database/users.js'
@@ -10,10 +10,33 @@ const userModel = new UserModel(pool)
 const cartModel = new CartModel(pool)
 const orderModel = new OrderModel(pool)
 
-const app = createApp({ productModel, userModel, cartModel, orderModel })
+import express, { json } from 'express'
+import { createProductsRouter } from './routes/products.js'
+import { createUsersRouter } from './routes/users.js'
+import { createCartsRouter } from './routes/carts.js'
+import { createOrdersRouter } from './routes/orders.js'
+import { mainRouter } from './routes/main.js'
+import { errorHandler } from './middlewares/errorHandler.js'
+import logger from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-try {
-  await startApp(app)
-} catch (error) {
-  console.error('Error starting the server', error)
-}
+
+dotenv.config()
+const app = express()
+app.use(logger('dev'))
+app.use(cors())
+app.use(json())
+app.use(errorHandler)
+app.disable('x-powered-by')
+
+app.use('/', mainRouter)
+app.use('/products', createProductsRouter({ productModel }))
+app.use('/users', createUsersRouter({ userModel }))
+app.use('/carts', createCartsRouter({ cartModel }))
+app.use('/orders', createOrdersRouter({ orderModel }))
+
+
+app.listen(process.env.PORT ?? 3000, () => {
+  console.log(`Server running on port http://localhost:${process.env.PORT ?? 3000}`)
+})
